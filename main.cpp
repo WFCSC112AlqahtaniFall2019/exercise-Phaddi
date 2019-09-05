b4ed1e6776a7216b737d79050dac0be218e5bf0c
ryanphadnis <phadrc18@wfu.edu>
2019-09-05 15:43:37 -0400

    In class exercise, reversed array, and converted to vector. Printed both.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     const int NUM_ELEMENTS = 8;   // Number of elements
-    int revVctr[8];               // User values
+    int revVctr[8];// User values
+    vector <int> reverseVector(NUM_ELEMENTS);
     int i;                        // Loop index
+    int temp; //for variable swapping
 
     cout << "Enter " << NUM_ELEMENTS << " integer values..." << endl;
     for (i = 0; i < NUM_ELEMENTS; ++i) {
@@ -12,14 +15,30 @@ int main() {
     }
 
     // Reverse
-    for (i = 0; i < NUM_ELEMENTS; ++i) {
-        revVctr[i] = revVctr[NUM_ELEMENTS - i]; // Swap
+    for (i = 0; i < NUM_ELEMENTS / 2; i++) {
+        temp = revVctr[i];
+        revVctr[i] = revVctr[NUM_ELEMENTS - i - 1];
+        revVctr[NUM_ELEMENTS - i - 1] = temp;
     }
 
     // Print values
-    cout << endl << "New values: ";
+    cout << endl << "New values in the form of an array:";
     for (i = 0; i < NUM_ELEMENTS; ++i) {
         cout << " " << revVctr[i];
+    }
+    cout << endl;
+
+    //change array to vector
+    for(i = 0; i < NUM_ELEMENTS; i++)
+    {
+        reverseVector.at(i) = revVctr[i];
+    }
+
+    //print values part 2
+    cout << "New values in the form of a vector: ";
+    for(i = 0; i < reverseVector.size(); i++)
+    {
+        cout << reverseVector.at(i) << " ";
     }
     cout << endl;
 
